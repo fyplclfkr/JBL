b71eff8fd8d3314b7d1eb30c24fa79c6243ce532
fyplclfkr <305725332@qq.com>
2024-01-16 10:17:51 +0800

    添加更新

---
+++ app/JBL_Updata.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class JBL_Updata(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        
+
+
+if __name__ == '__main__':
+    # enable dpi scale
+    QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+    
+    app = QApplication(sys.argv)
+    w = JBL_Updata()
+    w.show()
+    app.exec_()
